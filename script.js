639fe2a412e8d3de4342076fba60cb3e2da8c333
giovanebmr <giovanebmr@gmail.com>
2020-12-31 13:29:08 -0300

    adiciona gif e reload

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const background = document.querySelector('.background');
 let isJumping = false;
 let position = 0;
 let score = 0;
+let windowWidth = larguraTela() - 100;
+
+
+
+
+
+
+
+
+function larguraTela(){
+	return window.innerWidth;
+};
+
+window.addEventListener('resize', ()=>{
+	windowWidth = larguraTela() - 100;
+});
 
 const processaEvento = (event)=>{
 	//console.log("pressionou uma tecla qualquer...");
@@ -45,11 +61,11 @@ function jump(){
 
 function createCactus(){
 	const cactus = document.createElement('div');
-	let cactusPosition = 1000;
+	let cactusPosition = windowWidth;
 	let randomTime = Math.random() * 6000;
 	
 	cactus.classList.add('cactus');
-	cactus.style.left = 1000 + 'px';
+	cactus.style.left = windowWidth + 'px';
 	background.appendChild(cactus);
 	
 	let leftInterval = setInterval(()=>{
@@ -57,11 +73,15 @@ function createCactus(){
 			clearInterval(leftInterval);
 			background.removeChild(cactus);
 			upDateScore();
-			console.log(score);
 		}else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
 			//Game Over
 			clearInterval(leftInterval);
-			document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+			document.body.innerHTML = '' +
+				'<div class="game-over">' +
+					'<h1>Fim de jogo</h1>' +
+					'<p>Seu score: ' + score + '</p>' +
+					'<p><button onclick="location.reload()">reset</button></p>' +
+				'</div>';
 		}else{
 			cactusPosition -= 5;
 			cactus.style.left = cactusPosition + 'px';
@@ -79,6 +99,41 @@ function upDateScore(){
 	score++;
 	createScore();
 }
+
+
+
+
+
+
+
+/*referencia: http://www.stucox.com/blog/you-cant-detect-a-touchscreen/
+and
+https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
+*/
+function is_touch_device() {
+    if ("ontouchstart" in window || window.TouchEvent)
+        return true;
+
+    if (window.DocumentTouch && document instanceof DocumentTouch)
+        return true;
+
+    const prefixes = ["", "-webkit-", "-moz-", "-o-", "-ms-"];
+    const queries = prefixes.map(prefix => `(${prefix}touch-enabled)`);
+
+    return window.matchMedia(queries.join(",")).matches;
+}
+
+
+if( is_touch_device() === true ){
+	window.addEventListener('touchstart', ()=>{
+		if(!isJumping){
+				jump();
+			}
+	} )
+}
+
+
+
 
 
 createCactus();
